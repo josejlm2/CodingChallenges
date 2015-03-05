d7397bc7c3f0ffb48f4b975d71d9905c5c09dd40
Jose Manriquez <josejlm2@neo.tamu.edu>
2015-03-05 15:13:37 -0600

    AgeDistribution
    new lines

--- CodeEval/1.Easy/Scala/AgeDistributiion.scala
+++ CodeEval/1.Easy/Scala/AgeDistributiion.scala
@@ -1,31 +1,31 @@
 object Main extends App {
   def testAge(x: Int){
     if (x >= 0 & x<=2 ){
-        print("Still in Mama's arms");
+        println("Still in Mama's arms");
     }
     else if(x >=3 & x <= 4){
-         print("Preschool Maniac");
+         println("Preschool Maniac");
     }
     else if(x >=5 & x <= 11){
-         print("Elementary school");
+         println("Elementary school");
     }
     else if(x >=12 & x <= 14){
-         print("Middle school");
+         println("Middle school");
     }
     else if(x >=15 & x <= 18){
-         print("High school");
+         println("High school");
     }
     else if(x >=19 & x <= 22){
-         print("College");
+         println("College");
     }
     else if(x >=23 & x <= 65){
-         print("Working for the man");
+         println("Working for the man");
     }
     else if(x >=66 & x <= 100){
-         print("The Golden Years");
+         println("The Golden Years");
     }
     else{
-        print("This program is for humans");
+        println("This program is for humans");
     }
   }
     
