fd160bde93a60d6382db88b8e66af3e22f0136ae
Jose Manriquez <josejlm2@hotmail.com>
2015-06-28 12:46:39 -0500

    StepWise Word Code Eval challenge complete Ruby-Easy

---
+++ CodeEval/1.Easy/Ruby/StepwiseWord.rb
@@ -0,0 +1,28 @@
+def stepwise_words(sentence)
+	
+	#get the longest word
+	longestWord = ""
+	sentence.split.each{
+		|x|
+		if (x.length > longestWord.length)
+			longestWord = x
+		end
+	}
+	
+	longestWord.split("").each_with_index{
+		|x, i|
+		star = "*" * i
+		print "#{star}#{x} "
+	}
+	
+	puts "\n"
+end
+
+$line = "cat dog hello"
+$line1 = "stop football play"
+$line2 = "music is my life"
+#File.open(ARGV[0]).each_line do |line|
+	stepwise_words($line)
+	stepwise_words($line1)
+	stepwise_words($line2)
+#end
